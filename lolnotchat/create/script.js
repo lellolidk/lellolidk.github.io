2ee1fc8a73fd19ef5e8a33186954c90ca5b7a01e
lellolidk <104140174+lellolidk@users.noreply.github.com>
2024-04-09 21:31:08 +0200

    Update script.js

--- lolnotchat/create/script.js
+++ lolnotchat/create/script.js
@@ -1,81 +1,49 @@
 function sleep(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+values = {
+"animated" : 0,
+"badges" : 0,
+"special_badges" : 0,
+"bots" : 0,
+"commands" : 0
+}
+
+async function toggle(element){
+
+  if (values[element] == 0){
+    document.getElementById(element).style.background = "#00ffa2"
+    document.getElementById(element).style.backgroundImage = "url('check.svg')"
+    values[element] = 1
+  }
+  else{
+    document.getElementById(element).style.background = "#313131"
+    document.getElementById(element).style.backgroundImage = "none"
+    values[element] = 0
   }
   
-  values = {}
-  
-  async function toggle(element){
-  
-    if (values[element] == 0){
-      document.getElementById(element).style.background = "#00ffa2"
-      values[element] = 1
-    }
-    else{
-      document.getElementById(element).style.background = "#313131"
-      values[element] = 0
-    }
-    
-  }
-  
-  function createLink() {
-    values['channel'] = document.getElementById("channel").value;
-    values['size'] = document.getElementById("size").value;
-    values['shadow'] = document.getElementById("shadow").value;
-    values['background'] = document.getElementById("background").value;
-    values['font'] = document.getElementById("font").value;
-  
-    link = `https://lellolidk.github.io/lolnotchat?ch=${values['channel']}&si=${values['size']}&sh=${values['shadow']}&bg=${values['background']}&fo=${values['font']}&an=${values['animated']}&ba=${values['badges']}&sb=${values['special_badges']}&bo=${values['bots']}&co=${values['commands']}`;
-    return link;
-  }
-  
-  function copyLink(){
-    // Get the text field
-    var copyText = document.getElementById("output_link");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  }function sleep(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-  }
-  
-  values = {}
-  
-  async function toggle(element){
-  
-    if (values[element] == 0){
-      document.getElementById(element).style.background = "#00ffa2"
-      values[element] = 1
-    }
-    else{
-      document.getElementById(element).style.background = "#313131"
-      values[element] = 0
-    }
-    
-  }
-  
-  function createLink() {
-    values['channel'] = document.getElementById("channel").value;
-    values['size'] = document.getElementById("size").value;
-    values['shadow'] = document.getElementById("shadow").value;
-    values['background'] = document.getElementById("background").value;
-    values['font'] = document.getElementById("font").value;
-  
-    link = `https://lellolidk.github.io/lolnotchat?ch=${values['channel']}&si=${values['size']}&sh=${values['shadow']}&bg=${values['background']}&fo=${values['font']}&an=${values['animated']}&ba=${values['badges']}&sb=${values['special_badges']}&bo=${values['bots']}&co=${values['commands']}`;
-    return link;
-  }
-  
-  function copyLink(){
-    // Get the text field
-    var copyText = document.getElementById("output_link");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  }
+}
+
+function createLink() {
+  values['channel'] = document.getElementById("channel").value;
+  values['size'] = document.getElementById("size").value;
+  values['shadow'] = document.getElementById("shadow").value;
+  values['background'] = document.getElementById("background").value;
+  values['font'] = document.getElementById("font").value;
+
+  link = `https://lellolidk.de/lolnotchat?ch=${values['channel']}&si=${values['size']}&sh=${values['shadow']}&bg=${values['background']}&fo=${values['font']}&an=${values['animated']}&ba=${values['badges']}&sb=${values['special_badges']}&bo=${values['bots']}&co=${values['commands']}`;
+  return link;
+}
+
+function copyLink(){
+  // Get the text field
+  var copyText = document.getElementById("output_link");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+}
